5e77b1a094d8c1f90af0bbfcdb8ae79b20e56cee
Kajos <kajetan.szczurowski@gmail.com>
2024-09-12 22:11:56 +0200

    clerk initialized

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -1,5 +1,10 @@
 from flask import Flask
+from flask import request
 app = Flask(__name__)
+
+@app.route('/test', methods = ['GET'])
+def connection_test():
+    return 'all good, have fun'
 
 @app.route("/")
 def home():
--- FrontEnd/src/main.tsx
+++ FrontEnd/src/main.tsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+import { ClerkProvider } from '@clerk/clerk-react'
+
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  // <StrictMode>
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
     <App />
-  </StrictMode>,
+  </ClerkProvider>
+  // {/* </StrictMode> */}
 )
