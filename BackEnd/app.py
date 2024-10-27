f0358497713db37089893441d059b4dedb5a2557
Kajos <kajetan.szczurowski@gmail.com>
2024-10-27 20:22:59 +0100

    authentication and fetching started

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -1,11 +1,29 @@
 from flask import Flask
 from flask import request
+from flask import jsonify
+from clerk_backend_api import Clerk
+from os import getenv
+from dotenv import load_dotenv
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)
+load_dotenv()
+AUTHENTICATION_KEY = getenv("CLERK_SECRET_KEY")
+
 
 @app.route('/test', methods = ['GET'])
 def connection_test():
-    return 'all good, have fun'
+    return jsonify({'message': 'all good, have fun'})
 
 @app.route("/")
 def home():
-    return "Hello, Flasky!"
+    return jsonify({'message': 'all good, have fun'})
+
+auth = Clerk(
+    bearer_auth = AUTHENTICATION_KEY,
+)
+
+# res = auth.sessions.get(session_id="sess_2o1ZHAr9bs8VwkqGU60h6OUEsoe")
+# res2 = auth.clients.get(client_id = "client_2lz6lnoltIOZ6T3u4UqfUoAyRzT")
+# print(res3)
--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,9 +1,10 @@
-import { SignedIn, SignedOut, SignInButton, UserButton, useUser } from "@clerk/clerk-react";
+import { SignedIn, SignedOut, SignInButton, UserButton, useUser, useSession } from "@clerk/clerk-react";
 
 
 function App() {
 
   const {user} = useUser();
+  const {session} = useSession();
   console.log(user)
   return (
     <>
@@ -14,10 +15,18 @@ function App() {
       <SignedIn>
         <UserButton />
         Dzień Dobry
+        {session?.id}
+        <button onClick = {handleButton}>Fetch Test</button>
       </SignedIn>
 
     </>
   )
+
+  async function handleButton(){
+    const response = await fetch('http://127.0.0.1:5000');
+    const jsoned = await response.json();
+    console.log(jsoned);
+  }
 }
 
 export default App
