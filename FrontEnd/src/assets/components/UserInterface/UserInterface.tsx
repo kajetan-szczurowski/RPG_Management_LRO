3783de248a8c71fd0142042e5f9044e8a44b5282
Kajos <kajetan.szczurowski@gmail.com>
2024-10-28 21:13:07 +0100

    added background images

--- FrontEnd/src/assets/components/UserInterface/UserInterface.tsx
+++ FrontEnd/src/assets/components/UserInterface/UserInterface.tsx
@@ -3,7 +3,6 @@ import './user-interface.css'
 export default function UserInterface() {
     return(
         <>
-        <header className = 'main-header'></header>
         <main className = 'main-window'>
             <section className = 'left-side-container side-container'></section>
             <section className = 'right-side-container side-container'></section>
